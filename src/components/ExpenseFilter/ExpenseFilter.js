fd9ebaabf1299c27f8f6c909c77d9ed16b3de5c5
Nvegs <szencskyz13@gmail.com>
2022-01-16 21:03:21 +0100

    [added a new option in the select tag]

--- src/components/ExpenseFilter/ExpenseFilter.js
+++ src/components/ExpenseFilter/ExpenseFilter.js
@@ -19,6 +19,7 @@ return (
                     <option value="2020"></option>
                     <option value="2019"></option>
                     <option value="2018"></option>
+                    <option value="2017"></option>
                 </select>
             </div>
         </div>
